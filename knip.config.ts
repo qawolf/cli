6c4cad5e745b01503e8124deb735b4b284ad5244
Simon Ingeson <44818+smonn@users.noreply.github.com>
2026-05-05 14:19:25 -0400

    chore: move src/types.ts to ignore list in knip

--- knip.config.ts
+++ knip.config.ts
@@ -1,13 +1,10 @@
 import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
-  entry: [
-    "*.config.ts",
-    "src/types.ts",
-    "src/**/*.test.ts",
-    "src/**/*.mock.ts",
-  ],
+  entry: ["*.config.ts", "src/**/*.test.ts", "src/**/*.mock.ts"],
   project: ["src/**/*.ts"],
+  // temporarily ignore src/types.ts until it is in use in follow-ups
+  ignoreFiles: ["src/types.ts"],
 };
 
 export default config;
